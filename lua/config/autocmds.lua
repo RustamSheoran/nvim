3659048856104049ed173267f9a0eadb1e839d15
Rustam Sheoran <rustam98137@gmail.com>
2026-09-15 22:08:44 +0530

    made icpc tempalte as primary

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -13,17 +13,19 @@ vim.api.nvim_create_autocmd("BufReadPost", {
   },
   callback = function()
     -- Only trigger if entering a freshly created template file
-    if vim.fn.line("$") > 100 then
+    if vim.fn.line("$") >= 15 then
       local search_pattern = [[\vvoid\s+solve\s*\(\s*\)\s*\{]]
       local match_line = vim.fn.search(search_pattern, "nw")
       if match_line > 0 and vim.fn.line(".") == 1 then
         local line_text = vim.fn.getline(match_line)
         -- If clang-format collapsed the function into one line
         if line_text:match("}$") then
-           vim.fn.setline(match_line, "void solve() {")
-           vim.fn.append(match_line, {"  ", "}"})
+          vim.fn.setline(match_line, "void solve() {")
+          vim.fn.append(match_line, { "    ", "}" })
+        elseif vim.trim(vim.fn.getline(match_line + 1)) == "" then
+          vim.fn.setline(match_line + 1, "    ")
         end
-        vim.api.nvim_win_set_cursor(0, { match_line + 1, 2 })
+        vim.api.nvim_win_set_cursor(0, { match_line + 1, 4 })
         vim.cmd("startinsert!")
       end
     end
